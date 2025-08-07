68d096d2b177292d83748d51f8e7f99a2296ce42
kevinneung <94151024+kevinneung@users.noreply.github.com>
2025-08-06 22:41:39 -0700

    hackerrank practice, and cleaned dataset

---
+++ 4-Classification/1-Introduction/hackerRank_1_week_practice.py
@@ -0,0 +1,15 @@
+# import array as arr
+
+# myArray = arr.array('i', [-2, -1, 1, 2, 3])
+
+# listOfNegatives = [n for n in myArray if n < 0]
+# listOfPositives = [n for n in myArray if n > 0]
+# listOfZeros = [n for n in myArray if n == 0]
+
+# print(round(len(listOfNegatives)/len(myArray), 4))
+# print(round(len(listOfPositives)/len(myArray), 4))
+# print(round(len(listOfZeros)/len(myArray), 4))
+
+number = 12345
+for n in number:
+    print(n)
